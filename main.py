9357b253431cbb6a8e5c7451a29b72b3df56f6fc
Kiran Amaravati <kiran.k.varma@outlook.com>
2020-10-23 19:07:07 -0400

    Played around

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from mydefs import get_primes_and_composites, ask_for_num
 
 num1 = ask_for_num()
-num2 = ask_for_num()
+num2 = ask_for_num('Enter another one: ')
 print(get_primes_and_composites(num1, num2))
