38b2a6d834f9621fddc3361ef6d382466fc5c63b
Lightgoddess <aanya.varma@outlook.com>
2020-10-29 18:06:59 -0400

    Primes

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from mydefs import get_primes_and_composites, ask_for_num
+def get_primes_and_composites_dict():
+    num1 = ask_for_num()
+    num2 = ask_for_num()
+    result = get_primes_and_composites(num1, num2)
+    primes = result[0]
+    composites = result[1]
+    #print(f'primes:{primes}')
+    #print(f'composites:{composites}')
 
-num1 = ask_for_num()
-num2 = ask_for_num()
-print(get_primes_and_composites(num1, num2))
+    #foo = ([], [])
+    ID = {'primes_list':primes,
+    'composites_list':composites            }
+    return ID
+
+print(get_primes_and_composites_dict())      
