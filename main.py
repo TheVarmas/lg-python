bf89b93ff97257cbaf59fde8176c404f9ccc0331
Lightgoddess <aanya.varma@outlook.com>
2020-10-29 18:11:27 -0400

    primes

--- main.py
+++ main.py
@@ -7,16 +7,10 @@ def get_primes_and_composites_dict():
     composites = result[1]
     #print(f'primes:{primes}')
     #print(f'composites:{composites}')
-
-<<<<<<< HEAD
     #foo = ([], [])
+
     ID = {'primes_list':primes,
     'composites_list':composites            }
     return ID
 
 print(get_primes_and_composites_dict())      
-=======
-num1 = ask_for_num()
-num2 = ask_for_num('Enter another one: ')
-print(get_primes_and_composites(num1, num2))
->>>>>>> 37204bfbaf56d3d6e3d7f6ad3674f7721ad20949
