813198b64f7c4d68b97885f77fe0416d90a63f46
Lightgoddess <aanya.varma@outlook.com>
2020-10-31 21:12:46 +0000

    Printing cars that are a certain color

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
-from mydefs import ask_for_num
-print('1. Factorial')
-print('2. Sum')
-choice = ask_for_num('Enter your choice: ')
-if choice == 2:
-    n = ask_for_num()
-    result = 0
-    for i in range(1, n + 1):
-        result += i
-    print(result)
-elif choice == 1:
-    o = ask_for_num()
-    factorial = 1
-    for j in range(1, o + 1):
-            factorial = factorial * j
-    print(factorial)
+cars = {'grey':['car1'], 'blue':['car2', 'car3']}
+
+color = input('Enter a color: ')
+
+car_values = cars.get(color.lower(), []) # cars[color]
+
+if len(car_values) == 1:
+    print(f"{car_values} is {color}")
+elif len(car_values) > 1:
+    print(f"{car_values} are {color}")
 else:
-    print(f'{choice} is not a valid choice')    
+    print(f"No cars found with the color {color}")
