06d2e2cbd0ba3cf916a96efc24fa13fd174735f3
Lightgoddess <aanya.varma@outlook.com>
2020-10-31 20:21:44 +0000

    Implemented sum or factorial

--- main.py
+++ main.py
@@ -1,24 +1,18 @@
 from mydefs import ask_for_num
-
-n = ask_for_num()
-result = 1
-for i in range(1, n + 1):
-    result = result * i
-    #b = 1
-print(result)
-print(n)
-
-def a():
-    b = 2
-    print(b)
-
-a()
-print(n)
-
-def c():
-    b = 76
-    print(b)
-
-c()    
-result = "Topplehock"
-print(result)
+print('1. Factorial')
+print('2. Sum')
+choice = ask_for_num('Enter your choice: ')
+if choice == 2:
+    n = ask_for_num()
+    result = 0
+    for i in range(1, n + 1):
+        result += i
+    print(result)
+elif choice == 1:
+    o = ask_for_num()
+    factorial = 1
+    for j in range(1, o + 1):
+            factorial = factorial * j
+    print(factorial)
+else:
+    print(f'{choice} is not a valid choice')    
