969157accf5b3d987d95ee0cae6e44a516c850b3
Lightgoddess <aanya.varma@outlook.com>
2020-12-13 15:54:59 +0000

    Quiz now doesn't count invalid questions

--- main.py
+++ main.py
@@ -10,18 +10,24 @@ question_bag = [
         "answer": 2
     },
     {
-        "question":"Where does the sun set?",
-        "options":["North","East", "West", "South"], 
+        "question":"When is Canada day?",
+        "options":["June 2nd","August 20th", "July 1st", "November 3rd"], 
         "answer": 2
     }
 ]
 valid_options = ['a', 'b', 'C', 'd']
-option_separator = ':'
+option_separator = '.'
 score = 0
 qno = 0
 print("Welcome to the Quiz!")
-
+invalid_questions = 0
 while qno < len(question_bag):
+    if len(question_bag[qno]["options"]) != len(valid_options):
+        print("Something is wrong with this questsion or the options so I will skip it.")
+        qno += 1
+        invalid_questions += 1
+        continue
+
     print(question_bag[qno]["question"])
     print()
 
@@ -35,10 +41,11 @@ while qno < len(question_bag):
 
     if input_answer.upper() not in valid_options and input_answer.lower() not in valid_options:
         print('You entered an invalid answer')
+        continue
     elif input_answer.upper() == answer.upper():
         score += 1
 
     qno += 1
 
-print(f'Your score is {score} / {len(question_bag)}')
+print(f'Your score is {score} / {len(question_bag) - invalid_questions}')
 
